7e53eeaed4b283254192868d80ac2149436e7ebd
Elba Garcia <egarcias2021.net@gmail.com>
2024-11-29 00:03:31 -0500

    Implementacion de todas las historias de usuarios

--- registrarusuario.py
+++ registrarusuario.py
@@ -1,4 +1,5 @@
-#REGISTRO DE USUARIO.
+#REGISTRO DE USUARIO-
+#HISTORIA NUMERO 1
 def registrarUsuario():
     continuar = 's'
     
@@ -13,22 +14,25 @@ def registrarUsuario():
             numId = input("INGRESE SU NUMERO DE IDENTIFICACION: ").strip()
             
             
+            #SEGUNDA HISTORIA VALIDACION DEL CORREO ELECTRONICO
+            
             if "@" not in correo or "." not in correo:
                 print("EL CORREO ELECTRONICO NO ES VALIDO.")
                 continue
             
+            #TERCERA HISTORIA VALIDACION DE LA EDAD
     
             if not edad.isdigit() or int(edad) <= 0:
                 print("LA EDAD DEBE SER UN NUMERO POSITIVO.")
                 continue
             
-        
+            #HISTORIA 4, GUARDAR LOS DATOS EN UN ARCHIVO
             with open("USUARIOREGISTRADO.txt", "a") as archivo:
                 archivo.write(f"Nombre: {nombre}, Email: {correo}, Edad: {edad}, Tipo de ID: {tipoId}, Numero de Id: {numId}\n")
             
             print("¡REGISTRO EXITOSO! LOS DATOS HAN SIDO GUARDADOS")
             
-        
+            #HISTORIA 5 REGISTRAS MULTIPLES USUARIOS
             continuar = input("¿DESEA REGISTRAR OTRO USUARIO? (S/N): ").strip().lower()
         except Exception as e:
             print(f"Se produjo un error: {e}")
